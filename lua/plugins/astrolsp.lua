4177ec64fe667f6bfd8cded8a29d6d1b3ece15e2
catlowlevel <72902682+catlowlevel@users.noreply.github.com>
2024-09-28 22:19:14 +0800

    clangd-extensions

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -107,4 +107,42 @@ return {
       -- client.server_capabilities.semanticTokensProvider = nil
     end,
   },
+  {
+    "p00f/clangd_extensions.nvim",
+    lazy = true,
+    dependencies = {
+      "AstroNvim/astrocore",
+      opts = {
+        autocmds = {
+          clangd_extensions = {
+            {
+              event = "LspAttach",
+              desc = "Load clangd_extensions with clangd",
+              callback = function(args)
+                if assert(vim.lsp.get_client_by_id(args.data.client_id)).name == "clangd" then
+                  require "clangd_extensions"
+                  vim.api.nvim_del_augroup_by_name "clangd_extensions"
+                end
+              end,
+            },
+          },
+          -- clangd_extension_mappings = {
+          --   {
+          --     event = "LspAttach",
+          --     desc = "Load clangd_extensions with clangd",
+          --     callback = function(args)
+          --       if assert(vim.lsp.get_client_by_id(args.data.client_id)).name == "clangd" then
+          --         require("astrocore").set_mappings({
+          --           n = {
+          --             ["<Leader>lw"] = { "<Cmd>ClangdSwitchSourceHeader<CR>", desc = "Switch source/header file" },
+          --           },
+          --         }, { buffer = args.buf })
+          --       end
+          --     end,
+          --   },
+          -- },
+        },
+      },
+    },
+  },
 }
