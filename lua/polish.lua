41d793fd53bbe338b539f658dec68bba49155c3a
catlowlevel <72902682+catlowlevel@users.noreply.github.com>
2024-11-02 18:14:43 +0800

    update & set shell to pwsh on windows

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,3 +1,17 @@
+-- Define a function to check if a string starts with a given substring
+local function starts_with(str, start) return str:sub(1, #start) == start end
+
+-- Usage
+if starts_with(os.getenv "OS" or "", "Windows") then
+  vim.opt.shell = "pwsh"
+  vim.opt.shellcmdflag =
+    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+  vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+  vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
+end
+
 if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This will run last in the setup process and is a good place to configure
