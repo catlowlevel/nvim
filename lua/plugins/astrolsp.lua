0192781444fa8e607f763c9261f3e403a17850b8
catlowlevel <72902682+catlowlevel@users.noreply.github.com>
2026-08-08 23:18:08 +0800

    more jdtls setup

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -23,7 +23,7 @@ return {
           -- "go",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
-          -- "python",
+          "java",
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
--- lua/plugins/jdtls.lua
+++ lua/plugins/jdtls.lua
@@ -5,6 +5,37 @@ return {
     dependencies = {
       { "mfussenegger/nvim-jdtls" },
     },
+    opts = {
+      config = {
+        jdtls = {
+          settings = {
+            java = {
+              autobuild = {
+                enabled = false,
+              },
+            },
+          },
+        },
+      },
+      handlers = {
+        jdtls = function(server)
+          vim.lsp.config(server, {
+            root_dir = function(bufnr, on_dir)
+              local root = vim.fs.root(bufnr, { ".project", ".classpath" })
+              if root then on_dir(root) end
+            end,
+            settings = {
+              java = {
+                autobuild = {
+                  enabled = false,
+                },
+              },
+            },
+          })
+          vim.lsp.enable(server)
+        end,
+      },
+    },
   },
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
